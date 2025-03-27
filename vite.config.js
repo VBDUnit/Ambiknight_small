e0b917726904bdd3d969e8ca02934424c78f8214
VBDUnit <vbdunit@yandex.ru>
2025-03-28 01:35:50 +0500

    Новая сборка

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/Ambiknight_small/',
-  esbuild: {
-    loader: 'jsx',
-  },
-  
-   build: {
-    outDir: 'docs/dist',
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
+  build: {
+    outDir: 'docs',
   },
 });
